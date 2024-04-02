953bc12d28603c1cab97035e793867563931bf76
EVASHON <138779199+EVASHON@users.noreply.github.com>
2024-04-02 12:58:14 +0000

    "Ajout de bloquer vente

---
+++ sales.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class BlockSalesTab:
+    def __init__(self, notebook, db):
+        self.db = db
+        self.tab = ttk.Frame(notebook)
+        notebook.add(self.tab, text="Bloquer les ventes")
+        block_sales_label = tk.Label(self.tab, text="Entrer un user ID por bloquer les ventes:")
+        block_sales_label.pack()
+        self.block_sales_entry = tk.Entry(self.tab)
+        self.block_sales_entry.pack()
+        block_sales_button = tk.Button(self.tab, text="Bloquer les ventes", command=self.block_sales)
+        block_sales_button.pack()
+        self.block_sales_result_label = tk.Label(self.tab, text="")
+        self.block_sales_result_label.pack()
+
+    def block_sales(self):
+        user_id = self.block_sales_entry.get()
+        cursor = self.db.cursor()
+        query = "UPDATE oc_user SET status = 'blocked' WHERE user_id = %s"
+        cursor.execute(query, (user_id,))
+        self.db.commit()
+        
